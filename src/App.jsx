83ecc9ae6313c8e50866c76a5ca88dadd216c17c
Sereda Vitaliy <sereda.v484@gmail.com>
2025-11-18 23:06:39 +0200

    Создание приложения - Прогноз погоды- на React при помощи классов js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 class App extends React.Component {
-  render() {}
+  render() {
+    return (
+      <div className="app">
+        <h1>Прогноз погоды</h1>
+        <div>
+          <input type="text" />
+        </div>
+      </div>
+    );
+  }
 }
