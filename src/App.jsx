7887120fda3a08de675afdc18beac9f486149bd9
Sereda Vitaliy <sereda.v484@gmail.com>
2025-11-19 13:16:07 +0200

    Добавление состояний и отправку запросов к серверу

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,96 @@
 import React from "react";
 
+function getWeatherIcon(wmoCode) {
+  const icons = new Map([
+    [[0], "☀️"],
+    [[1], "🌤"],
+    [[2], "⛅️"],
+    [[3], "☁️"],
+    [[45, 48], "🌫"],
+    [[51, 56, 61, 66, 80], "🌦"],
+    [[53, 55, 63, 65, 57, 67, 81, 82], "🌧"],
+    [[71, 73, 75, 77, 85, 86], "🌨"],
+    [[95], "🌩"],
+    [[96, 99], "⛈"],
+  ]);
+  const arr = [...icons.keys()].find((key) => key.includes(wmoCode));
+  if (!arr) return "NOT FOUND";
+  return icons.get(arr);
+}
+
+function convertToFlag(countryCode) {
+  const codePoints = countryCode
+    .toUpperCase()
+    .split("")
+    .map((char) => 127397 + char.charCodeAt());
+  return String.fromCodePoint(...codePoints);
+}
+
+function formatDay(dateStr) {
+  return new Intl.DateTimeFormat("ru", {
+    weekday: "short",
+  }).format(new Date(dateStr));
+}
+
 class App extends React.Component {
+  constructor(prorps) {
+    super(prorps);
+    this.state = {
+      location: "lisbon",
+      isLoading: false,
+      dispalayLocation: "",
+      weather: {},
+    };
+    this.fetchWeather = this.fetchWeather.bind(this);
+  }
+
+  async fetchWeather() {
+    this.setState({ isLoading: true });
+    try {
+      // 1) Getting location (geocoding)
+      const geoRes = await fetch(
+        `https://geocoding-api.open-meteo.com/v1/search?name=${this.state.location}`
+      );
+      const geoData = await geoRes.json();
+      console.log(geoData);
+
+      if (!geoData.results) throw new Error("Location not found");
+
+      const { latitude, longitude, timezone, name, country_code } =
+        geoData.results.at(0);
+
+      this.setState({
+        dispalayLocation: `${name} ${convertToFlag(country_code)}`,
+      });
+
+      // 2) Getting actual weather
+      const weatherRes = await fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
+      );
+      const weatherData = await weatherRes.json();
+      this.setState({ weather: weatherData.daily });
+    } catch (err) {
+      console.err(err);
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  }
+
   render() {
     return (
       <div className="app">
         <h1>Прогноз погоды</h1>
         <div>
-          <input type="text" placeholder="Найти локацию..." />
+          <input
+            type="text"
+            placeholder="Найти локацию..."
+            value={this.state.location}
+            onChange={(e) => this.setState({ location: e.target.value })}
+          />
         </div>
+        <button onClick={this.fetchWeather}>Узнать погоду</button>
+
+        {this.state.isLoading && <p className="loader">Загрузка...</p>}
       </div>
     );
   }
